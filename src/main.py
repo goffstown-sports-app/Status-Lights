0005122bf25583b818dfd570fd95c3cd3288918e
Matthew Gleich <matthewgleich@gmail.com>
2019-09-08 22:30:21 -0400

    Fill main.py

--- src/main.py
+++ src/main.py
@@ -5,22 +5,46 @@ import datetime
 from time import sleep
 import RPi.GPIO as gpio_config
 
-# import database
+import database
+
 
 def main():
     """
     Runs the main file
     :return: None
     """
+    pins = {"Scrape-Calendar-Data": {"red": 4, "green": 17},
+            "Server-Monitor": {"red": 18, "green": 23}}
     gpio_config.setmode(gpio_config.BCM)
-    gpio_config.setup(17, gpio_config.OUT)
-    gpio_config.output(17, gpio_config.HIGH)
-    sleep(5)
-    gpio_config.output(17, gpio_config.LOW)
-    gpio_config.cleanup()
-
-
+    for service in pins:
+        for pin in pins[service].values():
+            gpio_config.setup(pin, gpio_config.OUT)
+            gpio_config.output(pin, gpio_config.HIGH)
+            sleep(0.2)
+            gpio_config.output(pin, gpio_config.LOW)
+    cred = credentials.Certificate("firestore_creds.json")
+    firebase_admin.initialize_app(
+        cred, {
+            "databaseURL": "https://ghs-app-5a0ba.firebaseio.com/",
+            'databaseAuthVariableOverride': {
+                'uid': 'my-service-worker'
+            }
+        })
+    pulse_amount = 0
+    second_diff = 2
+    database.set_monitoring_info(False, second_diff)
+    while True:
+        ref = db.reference("db-info/statuses")
+        for service in pins:
+            service_status = ref[service]["online"]
+            if service_status:
+                gpio_config.output(pins[service]["green"], GPIO.HIGH)
+                gpio_config.output(pins[service]["green"], GPIO.LOW)
+            else:
+                gpio_config.output(pins[service]["green"], GPIO.LOW)
+                gpio_config.output(pins[service]["green"], GPIO.HIGH)
+        pulse_amount += 1
+        database.update_pulse(pulse_amount, "Status-Lights")
 
 if __name__ == "__main__":
     main()
-
