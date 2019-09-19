3fe7f434b0dee3045524cafdf1327d51fc8b2d51
Matthew Gleich <matthewgleich@gmail.com>
2019-09-18 22:24:33 -0400

    Turn off at midnight

--- src/main.py
+++ src/main.py
@@ -34,17 +34,26 @@ def main():
     second_diff = 2
     database.set_monitoring_info(False, second_diff)
     while True:
+        current_time = datetime.datetime.now()
         ref = db.reference("db-info/statuses").get()
-        for service in pins:
-            service_status = ref[service]["online"]
-            if service_status:
-                gpio_config.output(pins[service]["green"], gpio_config.HIGH)
-                gpio_config.output(pins[service]["red"], gpio_config.LOW)
-            else:
+        if current_time.hour >= 23 and current_time.hour <= 5:
+            for service in pins:
                 gpio_config.output(pins[service]["green"], gpio_config.LOW)
-                gpio_config.output(pins[service]["red"], gpio_config.HIGH)
-        pulse_amount += 1
-        database.update_pulse(pulse_amount, "Status-Lights")
+                gpio_config.output(pins[service]["red"], gpio_config.LOW)
+            pulse_amount = 0
+            sleep(60)
+        else:
+            for service in pins:
+                service_status = ref[service]["online"]
+                if service_status:
+                    gpio_config.output(pins[service]["green"], gpio_config.HIGH)
+                    gpio_config.output(pins[service]["red"], gpio_config.LOW)
+                else:
+                    gpio_config.output(pins[service]["green"], gpio_config.LOW)
+                    gpio_config.output(pins[service]["red"], gpio_config.HIGH)
+            pulse_amount += 1
+            database.update_pulse(pulse_amount, "Status-Lights")
+            sleep(2)
 
 if __name__ == "__main__":
     main()
