901ff4e0bc8da0bab8a2c1c46da7f4f277cfb4aa
Matthew Gleich <matthewgleich@gmail.com>
2019-11-30 23:31:08 -0500

    Show lights

--- src/main.py
+++ src/main.py
@@ -5,8 +5,10 @@ from random import randint
 def main():
     blinkt.clear()
     blinkt.set_all(0, 250, 0, 1)
+    blinkt.show()
     sleep(0.01)
     blinkt.set_all(0, 250, 0, 0)
+    blinkt.show()
         
                 
 
