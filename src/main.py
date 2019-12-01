ee59414c7251b0c71adeed57805562da3cf13361
Matthew Gleich <matthewgleich@gmail.com>
2019-11-30 23:06:10 -0500

    Do more color testing

--- src/main.py
+++ src/main.py
@@ -1,28 +1,28 @@
 import blinkt
 from time import sleep
+from random import randint
 
 def main():
     blinkt.clear()
+    rgbVals = []
+    for i in range(8):
+        rgbVals.append([0, 0, 0])
     while True:
-        # Green on
-        for light in range(7):
-            blinkt.set_pixel(light, 0, 250, 0, 0.1)
-            blinkt.show()
-            sleep(0.5)
-        # Green off
-        for light in range(7):
-            blinkt.set_pixel(light, 0, 250, 0, 0)
-            blinkt.show()
-            sleep(0.5)
-        # Red on
-        for light in range(7):
-            blinkt.set_pixel(light, 250, 0, 0, 0.1)
-            blinkt.show()
-            sleep(0.5)
-        # Red off
-        for light in range(7):
-            blinkt.set_pixel(light, 250, 0, 0, 0)
-            blinkt.show()
-            sleep(0.5)
+        LED = randint(0, 7)
+        if randint(0, 1) == 0:
+            increase = False
+        else:
+            increase = True
+        selectedVal = rgbVals[LED][randint(0, 2)]
+        if selectedVal == 250 or not increase:
+            selectedVal -= 1
+        else:
+            selectedVal += 1
+        blinkt.set_pixel(LED, rgbVals[LED][0],
+                         rgbVals[LED][1], rgbVals[LED][2], 0.1)
+        blinkt.show()
+        sleep(0.01)
+        
+                
 
 main()
