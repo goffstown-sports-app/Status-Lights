67b338ee23008ffe2eca1e49bcba6a322f59bc58
Matthew Gleich <matthewgleich@gmail.com>
2019-11-30 22:20:13 -0500

    Show lights

--- src/main.py
+++ src/main.py
@@ -7,18 +7,22 @@ def main():
         # Green on
         for light in range(7):
             blinkt.set_pixel(light, 0, 250, 0, 0.1)
+            blinkt.show()
             sleep(0.5)
         # Green off
         for light in range(7):
             blinkt.set_pixel(light, 0, 250, 0, 0)
+            blinkt.show()
             sleep(0.5)
         # Red on
         for light in range(7):
             blinkt.set_pixel(light, 250, 0, 0, 0.1)
+            blinkt.show()
             sleep(0.5)
         # Red off
         for light in range(7):
             blinkt.set_pixel(light, 250, 0, 0, 0)
+            blinkt.show()
             sleep(0.5)
 
 main()
