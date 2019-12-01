17c4b569df94b17b7686213c589f30227a28403c
Matthew Gleich <matthewgleich@gmail.com>
2019-11-30 23:30:28 -0500

    Run test at start

--- src/main.py
+++ src/main.py
@@ -4,24 +4,9 @@ from random import randint
 
 def main():
     blinkt.clear()
-    rgbVals = []
-    for i in range(8):
-        rgbVals.append([0, 0, 0])
-    while True:
-        LED = randint(0, 7)
-        if randint(0, 1) == 0:
-            increase = False
-        else:
-            increase = True
-        selectedVal = rgbVals[LED][randint(0, 2)]
-        if selectedVal == 250 or not increase:
-            selectedVal -= 1
-        else:
-            selectedVal += 1
-        blinkt.set_pixel(LED, rgbVals[LED][0],
-                         rgbVals[LED][1], rgbVals[LED][2], 0.1)
-        blinkt.show()
-        sleep(0.01)
+    blinkt.set_all(0, 250, 0, 1)
+    sleep(0.01)
+    blinkt.set_all(0, 250, 0, 0)
         
                 
 
